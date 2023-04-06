8f5ac99b95b9a3d26577739424e46c09d889d850
Mobolaji Richard <lernenplatz@gmail.com>
2023-04-06 06:55:16 +0100

    init

---
+++ src/components/About.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  const cv_variants = {
+    hover: {
+      scale: 1.1,
+      transition: {
+        yoyo: Infinity,
+      },
+    },
+  };
+
+  const downloadCV = () => {
+    // using Java Script method to get PDF file
+    fetch("OginniMobolajiRichardResume.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "OginniMobolajiRichardResume.pdf";
+        alink.click();
+      });
+    });
+  };
+  return (
+    <div className="flex flex-col">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, React Native, Vue,
+        Node.js, and Express. I'm a quick learner and collaborate closely with
+        clients to create efficient, scalable, and user-friendly solutions that
+        solve real-world problems. Let's work together to bring your ideas to
+        life!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+      <motion.div
+      onClick={downloadCV}
+        variants={cv_variants}
+        whileHover="hover"
+        className="self-center mt-32 green-pink-gradient rounded-xl shadow-card px-10 p-5 cursor-pointer"
+      >
+        <p>Download CV</p>
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,387 @@
+import {
+  techtime,
+  lernen,
+  message,
+  rayda,
+  budget,
+  babyme,
+  artisto,
+  imggen,
+  paypal,
+  google,
+  mobile,
+  backend,
+  creator,
+  web,
+  dev,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  geeky,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Web Developer",
+    company_name: "DeveloperSpace",
+    icon: dev,
+    iconBg: "#383E56",
+    date: "Aug 2020 - Jan 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Assisted in the implementation of UI/UX designs provided by the design team.",
+      "Troubleshot and resolved frontend issues reported by users.",
+      "Developed and maintained internal tools and systems used by the company. ",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Software Developer",
+    company_name: "Geeky Experts Ltd.",
+    icon: geeky,
+    iconBg: "#E6DEDD",
+    date: "June 2022 - Present",
+    points: [
+      "Creation of the no-leak asset integrity cloud application user interface.",
+      "Implementation of mathematical relationships generated from the asset integrity theoretical concept into functional, readable codes. ",
+      "Collaborating with the backend team on code integration. ",
+      "Participated in the building, development of models and evaluation concepts for cloud-based asset integrity applications. ",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Richard is one of the most hardworking people i've ever seen.",
+    name: "Mrs Nsaka",
+    designation: "Vice President",
+    company: "NEHS, Akwa Ibom",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "He's a developer who truly cares about their clients' success.",
+    name: "Mr Richard",
+    designation: "HR",
+    company: "Geeky Experts",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "He's a fast learner, working with him is easy as he grabs things very easily ",
+    name: "Ahmed Adekunle",
+    designation: "CEO",
+    company: "A3 Construction",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Artisto",
+    description:
+      "Web-based platform that allows users to search different arts from all over the world, providing a musuem feeling for you at the cofort of your device.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: artisto,
+    source_code_link: "https://github.com/MobolajiRichard/Frontend-Developer-test",
+    web_link:"https://m-art-institute.netlify.app"
+  },
+  {
+    name: "Tech-Time",
+    description:
+      "Web application that sources for young talents around the world, with the aim of nurturing, teaching and mentoring them to become one of the best in the world  .",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: techtime,
+    source_code_link: "https://github.com/MobolajiRichard/techtalent",
+    web_link:"https://mobolaji-techtalent.netlify.app"
+  },
+  {
+    name: "Voiced Budget Tracker",
+    description:
+      "This a simplified app to track your income and expenses by using speech",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "speechly",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chartjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: budget,
+    source_code_link: "https://github.com/MobolajiRichard/budget_tracker",
+    web_link:"https://voicedbudgettracker.netlify.app"
+  },
+  {
+    name: "Lernen",
+    description:
+      "Website for a language learning institution based in Nigeria",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chartjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: lernen,
+    source_code_link: "https://github.com/",
+    web_link:"https://lernenplatz.netlify.app"
+  },
+  {
+    name: "Image Generator",
+    description:
+      "This is an AI image generator web application, it allows users to generate images by text",
+    tags: [
+      {
+        name: "vuejs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "open AI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: imggen,
+    source_code_link: "https://github.com/MobolajiRichard/image_generator",
+    web_link:"https://aiimagegenerator.netlify.app"
+  },
+  {
+    name: "BabyMe",
+    description:
+      "This is an e-commerce web application that allows users to purchase baby products from the comfort of their home.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "speechly",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chartjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: babyme,
+    source_code_link: "https://github.com/MobolajiRichard/babyMe---ecommerce",
+    web_link:"https://babymee.netlify.app"
+  },
+  {
+    name: "Auction",
+    description:
+      "This is an e-commerce web application that allows users to purchase baby products from the comfort of their home.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "speechly",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chartjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: rayda,
+    source_code_link: "https://github.com/MobolajiRichard/rayda_test",
+    web_link:"https://richard-rayda-test.netlify.app"
+  },
+  {
+    name: "Message Me",
+    description:
+      "This is a clone of the whatsapp web application, it allows users who log in to website to send an receive messages just like they would on their normal whatsapp.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "speechly",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chartjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: message,
+    source_code_link: "https://github.com/MobolajiRichard/MessagMe",
+    web_link:"https://mmessageme.netlify.app"
+  },
+  {
+    name: "Google Clone",
+    description:
+      "This is a clone of the popular search engine website Gogle, with all the search features included also using the google api.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "google api",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: google,
+    source_code_link: "https://github.com/MobolajiRichard/google__clone",
+    web_link:"https://google-clone-ruddy-zeta.vercel.app/"
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
